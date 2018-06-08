fec7a32ea4c7263e0f0aac0fa83c6f0da1d468de
alvaro <alvaro1332@gmail.com>
2018-06-08 19:24:16 +0200

    practica8

--- routes/index.js
+++ routes/index.js
@@ -140,4 +140,15 @@ router.delete('/quizzes/:quizId(\\d+)/tips/:tipId(\\d+)',
 router.get('/quizzes/randomplay',         quizController.randomplay);
 router.get('/quizzes/randomcheck/:quizId(\\d+)', quizController.randomcheck);
 
+//PRACTICA8
+router.get('/quizzes/:quizId(\\d+)/tips/:tipId(\\d+)/edit',
+    sessionController.loginRequired,
+    tipController.adminOrAuthorRequired,
+    tipController.edit);
+
+router.put('/quizzes/:quizId(\\d+)/tips/:tipId(\\d+)',
+    sessionController.loginRequired,
+    tipController.adminOrAuthorRequired,
+tipController.update);
+
 module.exports = router;
